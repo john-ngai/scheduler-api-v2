8235b5cfd7ceefe7ae9f8121238b275e42950584
mrjohnming <john.ngai@hotmail.com>
2024-02-17 13:13:03 -0500

    Move the indexRouter into it's own file exported from index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -3,12 +3,4 @@
  * All Rights Reserved
  */
 
-import express from 'express'
-
-const indexRouter = express.Router()
-
-indexRouter.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' })
-})
-
-export { indexRouter }
+export * from './indexRouter'
